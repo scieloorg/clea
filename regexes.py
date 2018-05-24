04aaec9ed3ab53830a5675e1d4e6dae38be881e0
Danilo J. S. Bellini <danilo.bellini@gmail.com>
2018-05-24 16:05:49 -0300

    Add fuzzy regexes in two distinct structures
    The goal of these regexes is to extract information from an article in
    its XML document format indirectly, from a single-line string with the
    "path" of each tag node in the tree
    
    There are two distinct single-line string descriptions, the tag path
    description is the full slash-separated tag names of an element and
    its ancestors, like:
    
      /article/front/article-meta/contrib-group/contrib
    
    Whereas the branch path description includes the attributes using the
    "tag@attribute=value" pattern without quotes (and removing all "@" and
    "/" symbols from the attribute name and value), like:
    
      /contrib@contrib-type=author/name/surname
      /aff@id=aff1/institution@content-type=orgname
    
    The FRONT_TAG_PATH_REGEXES should be applied on the full article tag
    paths to get information from the front matter (<front> element),
    while the BRANCH_REGEXES should be applied on an element-rooted path
    including the attributes
    
    The structure for the FRONT_TAG_PATH_REGEXES is
    
      {tag_name: regex, ...}
    
    which should match the path line of elements with the given tag_name
    (not its descendants), while the BRANCH_REGEXES structure is:
    
      {tag_name: [(custom_name, attribute_name, regex), ...], ...}
    
    where each triple has a regex that should match a descendant path line
    for an element that has an information to be directly extracted
    
    If attribute_name is an empty string, it means we should grab the full
    XML element inner text, else the desired information is in the
    element attribute that has the given name
    
    The custom_name is just a free name for the given "extractor" triple,
    so the result might be in a {custom_name: extracted_info, ...} format,
    
    (First commit)

---
+++ regexes.py
@@ -0,0 +1,138 @@
+import regex
+
+
+def bm_regex(regex_string):
+    """Compile best multiline regex."""
+    return regex.compile(regex_string, regex.B | regex.M)
+
+
+FRONT_TAG_PATH_REGEXES = {
+    "article-meta": regex.compile(
+        r"/(?:front){e<=1}"
+        r"/(?:.*/)?(?:article-meta){e<=2}$"
+    ),
+    "contrib": regex.compile(
+        r"/(?:front){e<=1}"
+        r"/(?:.*/)?(?:article-meta){e<=4}"
+        r"/(?:.*/)?(?:contrib){e<=2}$"
+    ),
+    "aff": regex.compile(
+        r"/(?:front){e<=1}"
+        r"/(?:.*/)?(?:article-meta){e<=4}"
+        r"/(?:.*/)?(?:aff){e<=1}$"
+    ),
+}
+
+BRANCH_REGEXES = {
+    "article-meta": [
+        ("article_doi", "", bm_regex(
+            r"/(?:article-id){e<=2}(?:@[^/]*)?"
+            r"@(?:pub-id-type){e<=4}"
+            r"=(?:doi){e<=1}(?:@[^/]*)?$"
+        )),
+        ("article_publisher_id", "", bm_regex(
+            r"/(?:article-id){e<=2}(?:@[^/]*)?"
+            r"@(?:pub-id-type){e<=4}"
+            r"=(?:publisher-id){e<=4}(?:@[^/]*)?$"
+        )),
+        ("article_title", "", bm_regex(r"/(?:article-title){e<=2}$")),
+    ],
+    "contrib": [
+        ("contrib_bio", "", bm_regex(r"/(?:bio){e<=1}$")),
+        ("contrib_degrees", "", bm_regex(r"/(?:degrees){e<=2}$")),
+        ("contrib_email", "", bm_regex(r"/(?:email){e<=1}$")),
+        ("contrib_name", "", bm_regex(r"/(?:name){e<=1}$")),
+        ("contrib_given_names", "", bm_regex(r"/(?:given-names){e<=3}$")),
+        ("contrib_orcid", "", bm_regex(
+            r"/(?:contrib-id){e<=2}(?:@[^/]*)?"
+            r"@(?:contrib-id-type){e<=7}"
+            r"=(?:orcid){e<=1}(?:@[^/]*)?$"
+        )),
+        ("contrib_prefix", "", bm_regex(r"/(?:prefix){e<=2}$")),
+        ("contrib_role", "", bm_regex(r"/(?:role){e<=1}$")),
+        ("contrib_suffix", "", bm_regex(r"/(?:suffix){e<=2}$")),
+        ("contrib_surname", "", bm_regex(r"/(?:surname){e<=2}$")),
+        ("contrib_type", "contrib-type", bm_regex(
+            r"/(?:contrib){e<=2}(?:@[^/]*)?"
+            r"@(?:contrib-type){e<=5}=[^/]*$"
+        )),
+        ("xref_corresp", "rid", bm_regex(
+            r"/(?:xref){e<=1}(?:@[^/]*)?"
+            r"@(?:ref-type){e<=2}"
+            r"=(?:corresp){e<=2}(?:@[^/]*)?$"
+        )),
+        ("xref_corresp_text", "", bm_regex(
+            r"/(?:xref){e<=1}(?:@[^/]*)?"
+            r"@(?:ref-type){e<=2}"
+            r"=(?:corresp){e<=2}(?:@[^/]*)?$"
+        )),
+        ("xref_aff", "rid", bm_regex(
+            r"/(?:xref){e<=1}(?:@[^/]*)?"
+            r"@(?:ref-type){e<=2}"
+            r"=(?:aff){e<=1}(?:@[^/]*)?$"
+        )),
+        ("xref_aff_text", "", bm_regex(
+            r"/(?:xref){e<=1}(?:@[^/]*)?"
+            r"@(?:ref-type){e<=2}"
+            r"=(?:aff){e<=1}(?:@[^/]*)?$"
+        )),
+    ],
+    "aff": [
+        ("addr_city", "", bm_regex(
+            r"/(?:city){e<=1}$|"
+            r"/(?:named-content){e<=4}(?:@[^/]*)?"
+            r"@(?:content-type){e<=4}"
+            r"=(?:city){e<=1}(?:@[^/]*)?$"
+        )),
+        ("addr_country", "", bm_regex(r"/(?:country){e<=2}(?:@[^/]*)?$")),
+        ("addr_country_code", "country", bm_regex(
+            r"/(?:country){e<=2}(?:@[^/]*)?"
+            r"@(?:country){e<=4}[=-][^/]*$"
+        )),
+        ("addr_postal_code", "", bm_regex(
+            r"/(?:postal-code){e<=3}$|"
+            r"/(?:named-content){e<=4}(?:@[^/]*)?"
+            r"@(?:content-type){e<=4}"
+            r"=(?:zipcode){e<=2}(?:@[^/]*)?$"
+        )),
+        ("addr_state", "", bm_regex(
+            r"/(?:state){e<=2}$|"
+            r"/(?:named-content){e<=4}(?:@[^/]*)?"
+            r"@(?:content-type){e<=4}"
+            r"=(?:state){e<=2}(?:@[^/]*)?$"
+        )),
+        ("aff_id", "id", bm_regex(
+            r"/(?:aff){e<=1}(?:@[^/]*)?"
+            r"@(?:id){e<=1}=[^/]*$"
+        )),
+        ("aff_text", "", bm_regex(r"/(?:aff){e<=1}(?:@[^/]*)?$")),
+        ("aff_email", "", bm_regex(r"/(?:email){e<=1}$")),
+        ("institution_original", "", bm_regex(
+            r"/(?:institution){e<=2}(?:@[^/]*)?"
+            r"@(?:content-type){e<=4}"
+            r"=(?:original){e<=2}(?:@[^/]*)?$"
+        )),
+        ("institution_orgdiv1", "", bm_regex(
+            r"/(?:institution){e<=2}(?:@[^/]*)?"
+            r"@(?:content-type){e<=4}"
+            r"=(?:orgdiv){e<=2}1(?:@[^/]*)?$"
+        )),
+        ("institution_orgdiv2", "", bm_regex(
+            r"/(?:institution){e<=2}(?:@[^/]*)?"
+            r"@(?:content-type){e<=4}"
+            r"=(?:orgdiv){e<=2}2(?:@[^/]*)?$"
+        )),
+        ("institution_orgname", "", bm_regex(
+            r"/(?:institution){e<=2}(?:@[^/]*)?"
+            r"@(?:content-type){e<=4}"
+            r"=(?:orgname){e<=2}(?:@[^/]*)?$"
+        )),
+        ("institution_orgname_rewritten", "", bm_regex(
+            r"/(?:institution){e<=2}(?:@[^/]*)?"
+            r"@(?:content-type){e<=4}"
+            r"=(?:normalized){e<=4}(?:@[^/]*)?$"
+        )),
+        ("label", "", bm_regex(r"/(?:label){e<=1}$")),
+        ("phone", "", bm_regex(r"/(?:phone){e<=1}$")),
+    ],
+}
