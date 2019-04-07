28b5b0d3ca89cdd5305fa97d3d6141859416fc27
Danilo J. S. Bellini <danilo.bellini@gmail.com>
2019-04-07 17:39:13 -0300

    Add a full outer join alternative without Pandas
    This new full outer join allows getting all data from aff/contrib
    pairs without data loss regarding "getting just the first string"
    like the former aff_contrib_inner_join used to do for the fields
    coming from neither <aff> nor <contrib>
    
    The actual changes are:
    
    - Rename aff_contrib_pairs_generator to aff_contrib_inner_gen
    - Add aff_contrib_full_gen, a generator of matching <aff>-<contrib>
      pairs (two Branch instances in a tuple), performed in a FULL OUTER
      JOIN style (yielding None instead of a Branch when there's no
      matching reference)
    - Add aff_contrib_full, which returns a "FULL OUTER JOIN" list of
      dictionaries, each containing the joined data_full from both
      branches of a pair coming from aff_contrib_full_gen
    - Add aff_contrib_inner, which returns an "INNER JOIN" list of
      dictionaries, each containing the joined data_full from both
      branches of a pair coming from aff_contrib_inner_gen
    - Restore the aff_contrib_inner_join behavior to get all fields from
      the "article-meta" and "journal-meta" regexes, instead of everything
      available; that's required since several new fields aren't prepared
      to be read in a strictly tabular format

--- clea/join.py
+++ clea/join.py
@@ -2,7 +2,6 @@ from collections import ChainMap
 from itertools import chain
 
 from .core import get_lev
-from .regexes import FRONT_TAG_PATH_REGEXES
 
 
 def aff_contrib_full_outer_join_dframe(article):
@@ -13,7 +12,7 @@ def aff_contrib_full_outer_join_dframe(article):
                     how="outer")
 
 
-def aff_contrib_pairs_generator(article):
+def aff_contrib_inner_gen(article):
     affs_ids = [get_lev(aff.node, "id") for aff in article.aff]
     contrib_rids = [[get_lev(xref, "rid")
                      for xref in contrib.get_field_nodes("xref_aff")]
@@ -25,12 +24,53 @@ def aff_contrib_pairs_generator(article):
                     yield aff, contrib
 
 
+def aff_contrib_full_gen(article):
+    affs_ids = [get_lev(aff.node, "id") for aff in article.aff]
+    contrib_rids = [[get_lev(xref, "rid")
+                     for xref in contrib.get_field_nodes("xref_aff")]
+                    for contrib in article.contrib]
+    contrib_missing = set(range(len(article.contrib)))
+    for aff_id, aff in zip(affs_ids, article.aff):
+        amiss = True
+        for cidx, (rid_list, contrib) in enumerate(zip(contrib_rids,
+                                                       article.contrib)):
+            for rid in rid_list:
+                if rid == aff_id:
+                    yield aff, contrib
+                    amiss = False
+                    contrib_missing.discard(cidx)
+        if amiss:
+            yield aff, None
+    for cidx in sorted(contrib_missing):
+        yield None, article.contrib[cidx]
+
+
+def aff_contrib_inner(article):
+    """Inner join list of matching <aff> and <contrib> entries."""
+    return [{**aff.data_full, **contrib.data_full}
+            for aff, contrib in aff_contrib_inner_gen(article)]
+
+
+def aff_contrib_full(article):
+    """Full outer join list of matching <aff> and <contrib> entries."""
+    return [{**(aff.data_full if aff else {}),
+             **(contrib.data_full if contrib else {}),
+            } for aff, contrib in aff_contrib_full_gen(article)]
+
+
 def aff_contrib_inner_join(article):
+    """Legacy inner join, returning items in tabular format as strings,
+    including the first matching entries from both article-meta
+    and journal-meta items.
+    """
     dicts = dict(ChainMap(*chain.from_iterable(
         [branch.data for branch in article.get(tag_name)]
-        for tag_name in FRONT_TAG_PATH_REGEXES
+        for tag_name in ["article-meta", "journal-meta"]
     )))
     return [{**dicts,
-             **{k: "; ".join(v) for k, v in aff.data_full.items()},
-             **{k: "; ".join(v) for k, v in contrib.data_full.items()},
-            } for aff, contrib in aff_contrib_pairs_generator(article)]
+             **{k: "; ".join(v) for k, v in aff_contrib},
+            } for aff_contrib in aff_contrib_inner(article)]
+
+
+# TODO: Remove this deprecated name
+aff_contrib_pairs_generator = aff_contrib_inner_gen
